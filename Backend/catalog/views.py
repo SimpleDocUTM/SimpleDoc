c4aacba6545a61814c2528135932b9c63a45e607
Trevor Nagy <1430911+trevornagy@users.noreply.github.com>
2020-09-29 11:42:36 -0400

    Base implementation for MCQ's. Created models & views & added to admin view.

--- Backend/catalog/views.py
+++ Backend/catalog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Quiz, QuizOption, QuizQuestion
+from .serializers import QuizSerializer, QuizOptionSerializer, QuizQuestionSerializer
 
 # Create your views here.
+class QuizViewSet(viewsets.ModelViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+class QuizOptionViewSet(viewsets.ModelViewSet):
+    queryset = QuizOption.objects.all()
+    serializer_class = QuizOptionSerializer
+
+class QuizQuestionViewSet(viewsets.ModelViewSet):
+    queryset = QuizQuestion.objects.all()
+    serializer_class = QuizQuestionSerializer
