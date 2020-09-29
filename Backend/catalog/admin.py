a155f46fb5291612c47695ba9b0bb0b9520fc6f5
Trevor Nagy <1430911+trevornagy@users.noreply.github.com>
2020-09-29 11:50:56 -0400

    Revert "Revert "Base implementation for MCQ's. Created models & views & added to admin view.""
    This reverts commit d5bf820c50aafeec48679e23b63ab8f6be7e90bf.

--- Backend/catalog/admin.py
+++ Backend/catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Quiz, QuizQuestion, QuizOption
 # Register your models here.
+
+admin.site.register(Quiz)
+admin.site.register(QuizQuestion)
+admin.site.register(QuizOption)
