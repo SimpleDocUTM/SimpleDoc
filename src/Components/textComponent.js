dd8bcd7bf17febde14eea335a57cc6ae9d4159d9
Shaheera Khalid <shaheera.khalid@mail.utoronto.cagit commit -mexitquitgit onfig --global user.email shaheera.khalid@mail.utoronto.ca>
2020-09-30 13:19:26 -0400

    textComponent.js

---
+++ src/Components/textComponent.js
@@ -0,0 +1,13 @@
+class textComponent extends React.Component {
+	render() {
+		return (
+		)
+	}
+}
+
+ReactDOM.render(<textComponent/>, document.getElementById('root'));
+const myelment = <textComponent text="n/a" />
+
+//takes in text ids as props
+//displays the text in card format
+
