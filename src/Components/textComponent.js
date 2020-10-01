570c0da141bb1841b4ca3a156c0715fbfc0a8e38
Shaheera Khalid <shaheera.khalid@mail.utoronto.cagit commit -mexitquitgit onfig --global user.email shaheera.khalid@mail.utoronto.ca>
2020-10-01 11:33:01 -0400

    text component

--- src/Components/textComponent.js
+++ src/Components/textComponent.js
@@ -1,13 +1,43 @@
-class textComponent extends React.Component {
-	render() {
-		return (
-		)
-	}
+import React from 'react';
+//import ReactDOM from 'react-dom'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+//import { makeStyles } from '@material-ui/core/styles';
+//import Typography from '@material-ui/core/Typography';
+import '../App.css';
+
+//const textStyles = makeStyles( {
+//        root: {
+//                minWidth: 275,
+//        },
+//        text: {
+//                fontSize: 14
+//        },
+//});
+
+class TextComponent extends React.Component{
+        render(){
+		const mystyle = {
+			cardcomponent:{
+				minWidth: 275,
+			},
+			cardcontent:{
+				fontSize: 14,
+			},
+			
+                 
+                }
+                return(
+                        <div>
+                                <Card style={mystyle.cardcomponent}>
+                                        <CardContent style={mystyle.cardcontent}>
+                                                        {this.props.text}
+                                        </CardContent>
+                                </Card>
+                        </div>
+                );
+        }
 }
 
-ReactDOM.render(<textComponent/>, document.getElementById('root'));
-const myelment = <textComponent text="n/a" />
-
-//takes in text ids as props
-//displays the text in card format
+export default TextComponent;
 
