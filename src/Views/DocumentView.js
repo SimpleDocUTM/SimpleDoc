85a1f5557bb01943b85c11933ea45fcdbe0af736
shadowshadow725 <shadowshadow725@gmail.com>
2020-10-01 18:22:59 -0400

    meeting

---
+++ src/Views/DocumentView.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class DocumentView extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>
+                text
+                </h1>
+
+
+                
+            </div>
+        )
+    };
+
+}
+
+export default DocumentView
---
+++ src/Views/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+
+class Login extends React.Component{
+    Validation = async (e) =>{
+        this.props.history.push('/DocumentView')
+    }
+    render(){
+        return(<div>
+                <form>
+                    <input type="text" id="username"/>
+                    <input type="password" id="password"/>
+                    <input type="submit" onClick={this.Validation}/>
+                    
+                </form>
+        </div>
+            );
+    }
+}
+
+
+
+export default Login;
