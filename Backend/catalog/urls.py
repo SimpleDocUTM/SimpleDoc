9ae3d140f010ab1b7ee7d490c31423ed398f936f
Trevor Nagy <1430911+trevornagy@users.noreply.github.com>
2020-10-07 22:59:46 -0400

    Add question submission.

--- Backend/catalog/urls.py
+++ Backend/catalog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('quizzes/', QuizListAPI.as_view()),
     path('quizzes/<int:pk>/', QuizDetailAPI.as_view()),
     path('quizzes/save-answer/', QuizOptionSubmissionAPI.as_view()),
-    path('user/<pk>/', UserInfoAPI.as_view())
+    path('user/<pk>/', UserInfoAPI.as_view()),
 
     path('documents/', DocumentationListAPI.as_view()),
     path('documents/<int:pk>/', DocumentationAPI.as_view()),
