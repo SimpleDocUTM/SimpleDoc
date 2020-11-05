b113ea80e76e34f3fc149288beb737616f16c1a1
Shaheera Khalid <shaheera.khalid@mail.utoronto.cagit commit -mexitquitgit onfig --global user.email shaheera.khalid@mail.utoronto.ca>
2020-11-05 08:56:48 -0500

    hard coded quizzes page, need to complete fetch

---
+++ src/Components/QuizCard.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import '../App.css';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
+import PropTypes from "prop-types";
+
+
+class QuizCard extends React.Component{
+	constructor(props) {
+        super(props);
+	}
+	
+    render(){
+		const mystyle = {
+			cardcomponent:{
+				minWidth: 275,
+			},
+			cardcontent:{
+				fontSize: 14,
+			},
+		}
+		const { title} = this.props;
+        return(
+            <div>
+                <Card style={mystyle.cardcomponent}>
+					<CardHeader
+						title="Concept Name"
+						//{title}
+        							//	subheader="September 14, 2016"
+      				/>
+                    <CardContent style={mystyle.cardcontent}>
+                        {title}
+                    </CardContent>
+					<CardActions>
+        				<Button size="small">Take Quiz</Button>
+      				</CardActions>
+                </Card>
+            </div>
+        );
+    }
+}
+
+QuizCard.propTypes = {
+    title: PropTypes.string,
+};
+
+export default QuizCard;
+
--- src/Views/QuizzesPage.js
+++ src/Views/QuizzesPage.js
@@ -2,15 +2,34 @@ import React from 'react';
 import Header from '../Components/Header';
 import NavBar from '../Components/NavBar';
 import Container from '@material-ui/core/Container';
-import CodeComponent from '../Components/CodeComponent';
 import styles from '../mystyle.module.css';
 import SimpleDocRest from "../api/SimpleDocRest";
+import QuizCard from '../Components/QuizCard';
 
 class QuizzesPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { quizTitle: ''};
+    }
 
+    fetchQuiz = async () => {
+        SimpleDocRest.get(`/quizzes/${this.state.quizId}/`)
+            .then((result) => {
+                this.setState({
+                    quizTitle: result.data.title,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
+    componentDidMount() {
+        this.fetchQuiz();
+    }
 
     render() {
+        const { quizTitle} = this.state;
         return (
             <div>
 		<Header />
@@ -18,11 +37,11 @@ class QuizzesPage extends React.Component {
                 {/* place holder */}
                 <Container maxWidth="lg">
 			<h1 className={styles.header}>
-                        CSC108
                     	</h1>
+			<QuizCard />
 			<h1 className={styles.header}>
-                        CSC148 
                         </h1>
+			<QuizCard />
 
 		</Container>
             </div>
