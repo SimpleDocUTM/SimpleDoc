e10adce36738d5bf2965ff35093881b2ec740a78
Trevor Nagy <1430911+trevornagy@users.noreply.github.com>
2020-12-02 01:25:33 -0500

    Added loading indicator to numerous pages.

--- src/Components/ConceptList.js
+++ src/Components/ConceptList.js
@@ -1,64 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import SimpleDocRest from "../api/SimpleDocRest";
-import { List, ListItem, ListItemText, ListSubheader } from '@material-ui/core/';
-import DocumentList from './DocumentList'
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  CircularProgress,
+} from "@material-ui/core/";
+import DocumentList from "./DocumentList";
+import Loading from "./Loading";
 
 export class ConceptList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      concepts: [],
+      conceptList: [],
+      isLoading: true,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            concepts: [],
-            conceptList: []
+  async componentDidMount() {
+    SimpleDocRest.get("concepts/").then((res) => {
+      this.setState({
+        concepts: res.data,
+      });
+      const ListItemStyle = {
+        padding: "5px",
+        margin: "120px 50px",
+        maxWidth: "100px",
+        float: "left",
+      };
+      var conceptList = [];
+      for (var i = 0; i < this.state.concepts.length; i++) {
+        if (this.state.concepts[i].category == this.props.category) {
+          conceptList.push(
+            <ListItem button key={i}>
+              <ListItemText
+                style={ListItemStyle}
+                primary={this.state.concepts[i]["name"]}
+              />
+              <DocumentList concept={this.state.concepts[i]["id"]} />
+            </ListItem>
+          );
+        } else if (this.props.category == "general" || !this.props.category) {
+          conceptList.push(
+            <ListItem button key={i}>
+              <ListItemText
+                style={ListItemStyle}
+                primary={this.state.concepts[i]["name"]}
+              />
+              <DocumentList concept={this.state.concepts[i]["id"]} />
+            </ListItem>
+          );
         }
-    }
+      }
+      this.setState({
+        conceptList: conceptList,
+        isLoading: false,
+      });
+    });
+  }
 
-    async componentDidMount() {
-        SimpleDocRest.get('concepts/').then((res) => {
-            this.setState({
-                concepts: res.data
-            })
-            const ListItemStyle = {
-                padding: "5px",
-                margin: "120px 50px",
-                maxWidth: "100px",
-                float: "left"
-            }
-            var conceptList = [];
-            for (var i = 0; i < this.state.concepts.length; i++) {
-                if (this.state.concepts[i].category == this.props.category) {
-                    conceptList.push(<ListItem button key={i}>
-                        <ListItemText style={ListItemStyle} primary={this.state.concepts[i]["name"]} />
-                        <DocumentList concept={this.state.concepts[i]["id"]} />
-                    </ListItem>);
-                } else if (this.props.category == 'general' || !this.props.category) {
-                    conceptList.push(<ListItem button key={i}>
-                        <ListItemText style={ListItemStyle} primary={this.state.concepts[i]["name"]} />
-                        <DocumentList concept={this.state.concepts[i]["id"]} />
-                    </ListItem>);
-                }
-            }
-            this.setState({
-                conceptList: conceptList
-            });
-        })
-    }
-
-    render() {
-
-
-        return (
-            <div>
-                <List
-                    component="nav"
-                    aria-labelledby="nested-list-subheader"
-                >
-                    {this.state.conceptList}
-
-                </List>
-            </div>
-        )
-    }
+  render() {
+    const { isLoading, conceptList } = this.state;
+    return (
+      <div>
+        <List component="nav" aria-labelledby="nested-list-subheader">
+          {isLoading ? <Loading /> : conceptList}
+        </List>
+      </div>
+    );
+  }
 }
 
-export default ConceptList
+export default ConceptList;
