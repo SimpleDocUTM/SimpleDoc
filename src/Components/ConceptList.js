95370a42126d28046a06025d54acb570990285a9
Osama <osama.hafez@mail.utoronto.ca>
2020-12-13 10:42:59 -0500

    fix loading indicator logic

--- src/Components/ConceptList.js
+++ src/Components/ConceptList.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 import SimpleDocRest from "../api/SimpleDocRest";
-<<<<<<< HEAD
-import { List, ListItem, ListItemText, ListSubheader, Typography } from '@material-ui/core/';
-import DocumentList from './DocumentList'
 import "./list.css";
-=======
 import {
   List,
   ListItem,
   ListItemText,
   ListSubheader,
   CircularProgress,
+  Typography
 } from "@material-ui/core/";
 import DocumentList from "./DocumentList";
 import Loading from "./Loading";
->>>>>>> 37241c9e54bfa28f24549fdba03635316eeb2d02
 
 export class ConceptList extends Component {
   constructor(props) {
@@ -27,97 +23,38 @@ export class ConceptList extends Component {
   }
 
   async componentDidMount() {
-    SimpleDocRest.get("concepts/").then((res) => {
+    SimpleDocRest.get('concepts/').then((res) => {
       this.setState({
-        concepts: res.data,
-      });
-      const ListItemStyle = {
-        padding: "5px",
-        margin: "120px 50px",
-        maxWidth: "100px",
-        float: "left",
-      };
+        concepts: res.data
+      })
       var conceptList = [];
       for (var i = 0; i < this.state.concepts.length; i++) {
         if (this.state.concepts[i].category == this.props.category) {
           conceptList.push(
-            <ListItem button key={i}>
-              <ListItemText
-                style={ListItemStyle}
-                primary={this.state.concepts[i]["name"]}
-              />
+            <div className="conceptList">
+              {/* <ListItem button key={i}> */}
+              {/* <ListItemText primary={<Typography variant="h5" className="conceptName">{this.state.concepts[i]["name"]}</Typography>} /> */}
+              {/* </ListItem> */}
+              <li>{this.state.concepts[i]["name"]}</li>
               <DocumentList concept={this.state.concepts[i]["id"]} />
-            </ListItem>
-          );
-        } else if (this.props.category == "general" || !this.props.category) {
-          conceptList.push(
-            <ListItem button key={i}>
-              <ListItemText
-                style={ListItemStyle}
-                primary={this.state.concepts[i]["name"]}
-              />
-              <DocumentList concept={this.state.concepts[i]["id"]} />
-            </ListItem>
-          );
+
+            </div>);
+        } else if (this.props.category == 'general' || !this.props.category) {
+          conceptList.push(<div className="conceptList">
+            {/* <ListItem button key={i}>
+                        <ListItemText primary={<Typography variant="h5" className="conceptName">{this.state.concepts[i]["name"]}</Typography>} />
+                    </ListItem> */}
+            <li>{this.state.concepts[i]["name"]}</li>
+            <DocumentList concept={this.state.concepts[i]["id"]} />
+          </div>);
         }
       }
       this.setState({
         conceptList: conceptList,
-        isLoading: false,
+        isLoading: false
       });
-    });
+    })
   }
-
-<<<<<<< HEAD
-    async componentDidMount() {
-        SimpleDocRest.get('concepts/').then((res) => {
-            this.setState({
-                concepts: res.data
-            })
-            var conceptList = [];
-            for (var i = 0; i < this.state.concepts.length; i++) {
-                if (this.state.concepts[i].category == this.props.category) {
-                    conceptList.push(
-                        <div className="conceptList">
-                            {/* <ListItem button key={i}> */}
-                            {/* <ListItemText primary={<Typography variant="h5" className="conceptName">{this.state.concepts[i]["name"]}</Typography>} /> */}
-                            {/* </ListItem> */}
-                            <li>{this.state.concepts[i]["name"]}</li>
-                            <DocumentList concept={this.state.concepts[i]["id"]} />
-
-                        </div>);
-                } else if (this.props.category == 'general' || !this.props.category) {
-                    conceptList.push(<div className="conceptList">
-                        {/* <ListItem button key={i}>
-                        <ListItemText primary={<Typography variant="h5" className="conceptName">{this.state.concepts[i]["name"]}</Typography>} />
-                    </ListItem> */}
-                        <li>{this.state.concepts[i]["name"]}</li>
-                        <DocumentList concept={this.state.concepts[i]["id"]} />
-                    </div>);
-                }
-            }
-            this.setState({
-                conceptList: conceptList
-            });
-        })
-    }
-
-    render() {
-
-
-        return (
-            <div className="listContainer">
-                <List
-                    component="nav"
-                    aria-labelledby="nested-list-subheader"
-                >
-                    {this.state.conceptList}
-
-                </List>
-            </div>
-        )
-    }
-=======
   render() {
     const { isLoading, conceptList } = this.state;
     return (
@@ -128,7 +65,6 @@ export class ConceptList extends Component {
       </div>
     );
   }
->>>>>>> 37241c9e54bfa28f24549fdba03635316eeb2d02
 }
 
 export default ConceptList;
--- src/Components/DocumentList.js
+++ src/Components/DocumentList.js
@@ -1,20 +1,9 @@
 import React, { Component } from "react";
 import SimpleDocRest from "../api/SimpleDocRest";
-<<<<<<< HEAD
 import { withRouter, Link, Redirect } from "react-router-dom"
 import { List, ListItem, ListItemText, ListSubheader, Collapse, Typography } from '@material-ui/core/';
 import "./list.css";
-=======
-import { withRouter, Link, Redirect } from "react-router-dom";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  ListSubheader,
-  Collapse,
-} from "@material-ui/core/";
 import Loading from "./Loading";
->>>>>>> 37241c9e54bfa28f24549fdba03635316eeb2d02
 
 export class DocumentList extends Component {
   constructor(props) {
@@ -30,90 +19,31 @@ export class DocumentList extends Component {
   }
 
   async componentDidMount() {
-    SimpleDocRest.get("/documents/").then((res) => {
+    SimpleDocRest.get('/documents/').then((res) => {
       this.setState({
-        documents: res.data,
-      });
-      const ListItemStyle = {
-        padding: "5px",
-        margin: "120px 50px",
-        maxWidth: "500px",
-      };
+        documents: res.data
+      })
+
       var listItems = [];
       // Replace if statement with a fetch by foreign key
       for (var i = 0; i < this.state.documents.length; i++) {
         if (this.state.concept == this.state.documents[i]["concept"]) {
           var id = this.state.documents[i]["id"];
           var l = `/DocumentView/${id}`;
-          listItems.push(
-            <Link to={l}>
-              <ListItem button key={i}>
-                <ListItemText
-                  style={ListItemStyle}
-                  primary={this.state.documents[i]["title"]}
-                  secondary={
-                    this.state.documents[i]["description"].slice(0, 100) + "..."
-                  }
-                />
-              </ListItem>
-            </Link>
-          );
+          listItems.push(<Link classes={{ root: "link" }} to={l} > <div className="listItem"><ListItem button key={i} >
+            <ListItemText primary={<Typography variant="h5" className="primary">{this.state.documents[i]["title"]}</Typography>} secondary={<Typography variant="h6" className="secondary">{this.state.documents[i]["description"].slice(0, 100) + "..."}</Typography>} />
+            <ListItemText classes={{ root: "primary" }} primary="Author" secondary={<Typography variant="h6" className="secondary">{this.state.documents[i]["contributor"]}</Typography>} />
+          </ListItem ></div>
+          </Link >);
         }
       }
       this.setState({
         listItems: listItems,
-        isLoading: false,
-      });
-    });
+        isLoading: false
+      })
+    })
   }
 
-<<<<<<< HEAD
-    async componentDidMount() {
-        SimpleDocRest.get('/documents/').then((res) => {
-            this.setState({
-                documents: res.data
-            })
-
-            var listItems = [];
-            // Replace if statement with a fetch by foreign key
-            for (var i = 0; i < this.state.documents.length; i++) {
-                if (this.state.concept == this.state.documents[i]["concept"]) {
-                    var id = this.state.documents[i]["id"];
-                    var l = `/DocumentView/${id}`;
-                    listItems.push(<Link classes={{ root: "link" }} to={l} > <div className="listItem"><ListItem button key={i} >
-                        <ListItemText primary={<Typography variant="h5" className="primary">{this.state.documents[i]["title"]}</Typography>} secondary={<Typography variant="h6" className="secondary">{this.state.documents[i]["description"].slice(0, 100) + "..."}</Typography>} />
-                        <ListItemText classes={{ root: "primary" }} primary="Author" secondary={<Typography variant="h6" className="secondary">{this.state.documents[i]["contributor"]}</Typography>} />
-                    </ListItem ></div>
-                    </Link >);
-                }
-            }
-            this.setState({
-                listItems: listItems
-            })
-        })
-    }
-
-
-    render() {
-        return (
-            <div>
-                {/* {this.renderRedirect()} */}
-                <List
-                    component="nav"
-                    aria-labelledby="nested-list-subheader"
-                    subheader={
-                        <ListSubheader component="div" id="nested-list-subheader">
-                        </ListSubheader>
-                    }
-                >
-                    {this.state.listItems}
-
-                </List>
-                {/* </Collapse> */}
-            </div >
-        )
-    }
-=======
   render() {
     const { isLoading, listItems } = this.state;
     return (
@@ -135,7 +65,7 @@ export class DocumentList extends Component {
       </div>
     );
   }
->>>>>>> 37241c9e54bfa28f24549fdba03635316eeb2d02
+
 }
 
 export default withRouter(DocumentList);
